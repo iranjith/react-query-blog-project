c478df22795736fc1b402db3a2ed20a93251a346
Ranjith.Kumar <theranjith@gmail.com>
2025-07-06 22:02:35 +0530

    added pagination feature

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -6,14 +6,13 @@ import { useQuery } from "@tanstack/react-query";
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
   const { data, isLoading, isError } = useQuery({
-    queryKey: ["posts"],
-    queryFn: fetchPosts,
-    staleTime: 2000, //2seconds
+    queryKey: ["posts", currentPage],
+    queryFn: () => fetchPosts(currentPage),
+    staleTime: 2000,
   });
 
   if (isLoading) {
@@ -38,11 +37,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((previousVal) => previousVal - 1);
+          }}
+        >
           Previous page
         </button>
         <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((previousVal) => previousVal + 1);
+          }}
+        >
           Next page
         </button>
       </div>
